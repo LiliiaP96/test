fc6df5cee11413eb544dff5a81b49b6629f91bad
LiliiaP96 <lilyapetryshyn96@gmail.com>
2022-07-08 19:46:33 -0400

    That is my test commit

--- src/test/java/ui_automation/step_definitions/MBExpensesSteps.java
+++ src/test/java/ui_automation/step_definitions/MBExpensesSteps.java
@@ -24,6 +24,7 @@ import java.util.List;
 
 public class MBExpensesSteps {
 
+    //That is my comment
     MBExpensesPage mbExpensePage = new MBExpensesPage();
     static final Logger log = LogManager.getLogger(MBExpensesSteps.class);
 
